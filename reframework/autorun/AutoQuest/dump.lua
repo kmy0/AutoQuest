ca9de8e86a19e561a43045811c06103a1e9f27be
kmy0 <93156259+kmy0@users.noreply.github.com>
2022-09-09 15:33:56 +0200

    Update dump.lua

--- reframework/autorun/AutoQuest/dump.lua
+++ reframework/autorun/AutoQuest/dump.lua
@@ -5,7 +5,6 @@ local methods
 local config
 local functions
 
-local quest_cat_list = {}
 
 dump.ed = false
 dump.no_of_quests = 0
@@ -36,31 +35,16 @@ dump.ranks_ids = {
 }
 dump.quest_data_list = {}
 local quest_categories = {
+                    Arena=6,
                     Kingdom=11,
                     Kyousei=10,
                     Mystery=12,
                     ServantRequest=9,
 }
 
-local function get_quest_category(quest_no)
-    local cat = nil
-    local lvl = nil
-    for k,v in pairs(quest_cat_list) do
-        if v[quest_no] then
-            cat = k
-            lvl = v[quest_no]
-            break
-        end
-    end
-    return cat,lvl
-end
-
-local function parse_quest_data(quest_data,event_ids,random_mystery)
+local function parse_quest_data(quest_data)
     local no = nil
-    local target_type = {}
-    local multi_monster = false
-    local single_monster = false
-    local small_monster = false
+    local monster_hunt_type = ''
     local quest_cat = nil
     local quest_level = nil
     local highrank_unlock = methods.is_hr_unlocked:call(singletons.progquestman)
@@ -68,27 +52,25 @@ local function parse_quest_data(quest_data,event_ids,random_mystery)
     local type = nil
     local rank = nil
     local online = false
+    local target_num = 0
 
     for no,quest in pairs(quest_data) do
 
-        quest_cat,quest_level = get_quest_category(no)
+        quest_cat = quest['category']
+        quest_level = quest['level']
+        quest = quest['data']
 
         online = false
+        monster_hunt_type = 'none'
+        target_num = 0
         type = quest:get_field("_QuestType")
 
-        if not quest_cat then
-            if event_ids[no] then
-                quest_cat = 'Event'
+        if type == dump.quest_types['HYAKURYU'] then
+            quest_cat = 'Rampage'
+        end
 
-            elseif random_mystery[no] then
-                quest_cat = 'Random Mystery'
-            elseif type == dump.quest_types['ARENA'] then
-                quest_cat = 'Arena'
-            elseif type == dump.quest_types['HYAKURYU'] then
-                quest_cat = 'Rampage'
-            else
-                quest_cat = 'Normal'
-            end
+        if not quest_cat then
+            quest_cat = 'Normal'
         end
 
         if quest_cat ~= 'Random Mystery'
@@ -103,21 +85,24 @@ local function parse_quest_data(quest_data,event_ids,random_mystery)
         end
 
         if quest_cat == 'Rampage' then
-            multi_monster = 'Unknown'
-            single_monster = 'Unknown'
-            small_monster = 'Unknown'
+            monster_hunt_type = 'multi'
         elseif quest_cat == 'Random Mystery' then
-            single_monster = true
-        else
-            local tt = quest:get_field('_TargetType')
-            target_type.a = tt:get_element(0):get_field("value__")
-            target_type.b = tt:get_element(1):get_field("value__")
-            if target_type.b ~= 0 or target_type.a == 5 then
-                multi_monster = true
-            elseif target_type.a == 6 then
-                small_monster = true
-            elseif target_type.a < 5 and target_type.a > 1 then
-                single_monster = true
+            if quest:get_field('_HuntTargetNum') > 1 then
+                monster_hunt_type = 'multi'
+            else
+                monster_hunt_type = 'single'
+            end
+        elseif type ~= dump.quest_types['TOUR'] and type ~= dump.quest_types['COLLECTS'] then
+
+            target_num = quest:get_field('_TgtNum'):get_elements()
+            target_num = target_num[1]:get_field('mValue') + target_num[2]:get_field('mValue')
+
+            if target_num == 1 then
+                monster_hunt_type = 'single'
+            elseif target_num > 5 then
+                monster_hunt_type = 'small'
+            elseif target_num > 1 then
+                monster_hunt_type = 'multi'
             end
         end
 
@@ -129,68 +114,62 @@ local function parse_quest_data(quest_data,event_ids,random_mystery)
             end
             quest_level = quest:get_field("_QuestLv")
             type = dump.quest_types['HYAKURYU']
-            completed = false
         else
             rank = quest:get_field("_EnemyLv")
-            completed = methods.is_quest_clear:call(singletons.progquestman,no)
         end
+
+        completed = methods.is_quest_clear:call(singletons.progquestman,no)
 
         if quest_cat == 'Event' and quest:get_field("_EnemyLv") == dump.ranks_ids['High'] and not highrank_unlock then
             unlocked = false
+        elseif quest_cat == 'Event' then
+            unlocked = true
         else
             unlocked = methods.is_quest_unlocked:call(singletons.progquestman,no)
         end
+
         dump.quest_data_list[no] = {
                         type=type,
                         rank=rank,
                         level=quest_level,
                         category=quest_cat,
-                        small_monster=small_monster,
-                        single_monster=single_monster,
-                        multi_monster=multi_monster,
+                        monster_hunt_type=monster_hunt_type,
                         unlocked=unlocked,
                         completed=completed,
                         online=online
                         }
 
-        multi_monster = false
-        single_monster = false
-        small_monster = false
     end
 end
 
 function dump.random_mystery()
-    local random_mystery = {}
     local quest_data = {}
     for _,quest in pairs(singletons.questman:get_field('_RandomMysteryQuestData'):get_elements()) do
         no = quest:get_field("_QuestNo")
-        quest_data[no] = quest
-        random_mystery[no] = 1
+        if no ~= 0 and no ~= -1 then
+            quest_data[no] = {data=quest,category='Random Mystery'}
+        end
     end
     for i=0,120 do
         dump.quest_data_list[700000 + i] = nil
     end
-    parse_quest_data(quest_data,{},random_mystery)
+    parse_quest_data(quest_data)
 end
 
 function dump.quest_data()
     local no = nil
     local quest_data = {}
     local quest_dict = singletons.questman:get_field("_QuestDataDictionary"):get_field('_entries'):get_elements()
-    local event_ids = {}
-    local random_mystery = {}
-    quest_cat_list = {}
     dump.quest_data_list = {}
 
     for k,v in pairs(quest_categories) do
-        quest_cat_list[k] = {}
         for i=0,7 do
             lst = methods.get_quest_no_array:call(singletons.questman,v,i)
             if lst then
                 lst = lst:get_elements()
                 for _,e in pairs(lst) do
                     no = e:get_field("value__")
-                    quest_cat_list[k][no] = i
+                    quest_data[no] = {category=k,level=i}
                 end
             end
         end
@@ -199,35 +178,28 @@ function dump.quest_data()
     for _,k in pairs(quest_dict) do
         no = k:get_field('key')
         if no ~= 0 and no ~= -1 then
-            quest_data[no] = k:get_field('value'):get_field('<RawNormal>k__BackingField')
+            if not quest_data[no] then
+                quest_data[no] = {}
+            end
+            quest_data[no]['data'] = k:get_field('value'):get_field('<RawNormal>k__BackingField')
         end
     end
 
     for _,quest in pairs(singletons.questman:get_field('_DlQuestData'):get_field("_Param"):get_elements()) do
         no = quest:get_field("_QuestNo")
         if no ~= 0 and no ~= -1 then
-            event_ids[no] = 1
+            quest_data[no]['category'] = 'Event'
         end
     end
 
     for _,quest in pairs(singletons.questman:get_field('_RandomMysteryQuestData'):get_elements()) do
         no = quest:get_field("_QuestNo")
         if no ~= 0 and no ~= -1 then
-            quest_data[no] = quest
-            random_mystery[no] = 1
+            quest_data[no] = {data=quest,category='Random Mystery'}
         end
     end
 
-    -- local rampage_ids = {}
-    -- for _,quest in pairs(singletons.questman:get_field('_HyakuryuQuestData'):get_elements()) do
-    --     no = quest:get_field("_QuestNo")
-    --     if no ~= 0 and no ~= -1 then
-    --         quest_data[no] = quest
-    --         rampage_ids[no] = 1
-    --     end
-    -- end
-
-    parse_quest_data(quest_data,event_ids,random_mystery)
+    parse_quest_data(quest_data)
 
     if not next(dump.non_custom_quest_ids) then
 
@@ -245,7 +217,7 @@ function dump.quest_data()
 end
 
 function dump.load()
-	dump.non_custom_quest_ids = json.load_file(config.non_custom_quest_ids_file_name)
+	dump.non_custom_quest_ids = json.load_file(dump.non_custom_quest_ids_file_name)
 	if not dump.non_custom_quest_ids then dump.non_custom_quest_ids = {} end
     dump.anomaly_investigations_main_monsters = json.load_file(dump.anomaly_investigations_main_monsters_file_name)
     if not dump.anomaly_investigations_main_monsters then
