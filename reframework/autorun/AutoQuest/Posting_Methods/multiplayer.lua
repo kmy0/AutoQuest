d1fb8a0372aa670727a183e414583121353ea58d
kmy0 <93156259+kmy0@users.noreply.github.com>
2022-12-08 02:48:51 +0100

    Update multiplayer.lua

--- reframework/autorun/AutoQuest/Posting_Methods/multiplayer.lua
+++ reframework/autorun/AutoQuest/Posting_Methods/multiplayer.lua
@@ -5,43 +5,48 @@ local methods
 local functions
 local vars
 local dump
+local randomizer
 
 local menu_list_type_def = sdk.find_type_definition('snow.gui.SnowGuiCommonBehavior.MenuListCtrlBase')
-local quest_counter_type_def = sdk.find_type_definition('snow.gui.fsm.questcounter.GuiQuestCounterFsmManager')
 
-local loop_count = 0
-local loop_max = 100
-local target_name = nil
-local get_menu = false
-local session_action_active = false
-local req_select_attempt = false
-
+local quest_counter_obj_loop = {
+    max=100,
+    count=0,
+    name=nil,
+    sensor=nil
+}
+local actions = {
+    select_quest_counter=false,
+    select_menu=false,
+    select_option=false,
+    select_mystery=false
+}
 local quest_counter_obj_ids = {
-            [0]='nid002', --village
-            [3]='nid102', --hub
-            [6]='nid601'  --elgado
+    [0]='nid002', --village
+    [3]='nid102', --hub
+    [6]='nid601'  --elgado
 }
 local quest_counter_top_menu_types = {
-                        [7]=true, --Arena
-                        [8]=true, --Challenge
-                        [5]=true, --Event
-                        [1]=true, --Normal_Hall_High
-                        [20]=true, --Normal_Hall_HighLow
-                        [2]=true, --Normal_Hall_Low
-                        [12]=true, --Normal_Hall_Master
-                        [4]=true, --Training
-                        [13]=false --Mystery
+    [7]="Normal", --Arena
+    [8]="Normal", --Challenge
+    [5]="Normal", --Event
+    [1]="Normal", --Normal_Hall_High
+    [20]="Normal", --Normal_Hall_HighLow
+    [2]="Normal", --Normal_Hall_Low
+    [12]="Normal", --Normal_Hall_Master
+    [4]="Normal", --Training
+    [13]='Random Mystery' --Mystery
 }
 
 local function select_mystery_quest()
-    local quest_counter_singleton = sdk.get_managed_singleton('snow.gui.fsm.questcounter.GuiQuestCounterFsmManager')
-    local menu_list = methods.get_active_menu_quest_list:call(quest_counter_singleton)
-    local menu_list_size = menu_list:get_field('mSize')
-    vars.cursor = quest_counter_type_def:get_field('<QuestMenuCursor>k__BackingField'):get_data()
-    for i=0,menu_list_size do
-        local quest = menu_list:call('get_Item',i)
-        local quest_no = quest:get_field('<RandomMystery>k__BackingField'):get_field('_QuestNo')
-        if quest_no == tonumber(config.current.auto_quest.quest_no) then
+    local menu = {}
+    menu.list = methods.get_active_menu_quest_list:call(singletons.quest_counter)
+    menu.size = menu.list:get_field('mSize')
+    vars.cursor = singletons.quest_counter:get_field('<QuestMenuCursor>k__BackingField')
+
+    for i=0,menu.size-1 do
+        local quest = menu.list:get_Item(i)
+        if quest:get_field('<RandomMystery>k__BackingField'):get_field('_QuestNo') == tonumber(config.current.auto_quest.quest_no) then
             methods.menu_list_cursor_set_index:call(vars.cursor,i)
             vars.selection_trigger = i
             return true
@@ -50,26 +55,18 @@ local function select_mystery_quest()
     return false
 end
 
-local function get_quest_counter_menu()
-    local quest_counter_menu_list = quest_counter_type_def:get_field('<QuestCounterTopMenuList>k__BackingField'):get_data()
-    vars.cursor = quest_counter_type_def:get_field('<TopMenuCursor>k__BackingField'):get_data()
-    local quest_counter_menu_list_size = quest_counter_menu_list:get_field('mSize')
-    local cursor_index = methods.menu_list_cursor_get_index:call(vars.cursor)
-    local menu_id = quest_counter_menu_list:call('get_Item',cursor_index)
-    local bool = nil
+local function get_quest_counter_top_menu()
+    local menu = {}
+    menu.list = singletons.quest_counter:get_field('<QuestCounterTopMenuList>k__BackingField')
+    vars.cursor = singletons.quest_counter:get_field('<TopMenuCursor>k__BackingField')
+    menu.size = menu.list:get_field('mSize')
+    menu.cursor_index = methods.menu_list_cursor_get_index:call(vars.cursor)
+    menu.id = menu.list:get_Item(menu.cursor_index)
 
-    if dump.quest_data_list[tonumber(config.current.auto_quest.quest_no)]['category'] == 'Random Mystery' then
-        vars.quest_type = 'Random Mystery'
-        bool = false
-    else
-        vars.quest_type = "Normal"
-        bool = true
-    end
-
-    if quest_counter_top_menu_types[menu_id] ~= bool then
-        for i=0,quest_counter_menu_list_size-1 do
-            menu_id = quest_counter_menu_list:call('get_Item',i)
-            if quest_counter_top_menu_types[menu_id] == bool then
+    if quest_counter_top_menu_types[menu.id] ~= vars.quest_type then
+        for i=0,menu.size-1 do
+            menu.id = menu.list:get_Item(i)
+            if quest_counter_top_menu_types[menu.id] == vars.quest_type then
                 methods.menu_list_cursor_set_index:call(vars.cursor,i)
                 vars.selection_trigger = i
                 return true
@@ -91,23 +88,24 @@ end
 
 function multiplayer.switch()
     function functions.post_quest()
-        local random_pool = true
         if methods.can_open_quest_board:call(singletons.guiman) then
             if not methods.is_quest_posted:call(singletons.questman) then
-                target_name = quest_counter_obj_ids[ singletons.vilman:get_field('<_CurrentAreaNo>k__BackingField') ]
-                if target_name then
-                    if config.current.auto_quest.auto_randomize then
-                        random_pool = randomizer.roll()
+                quest_counter_obj_loop.name = quest_counter_obj_ids[ singletons.vilman:get_field('<_CurrentAreaNo>k__BackingField') ]
+                if quest_counter_obj_loop.name then
+                    if config.current.auto_quest.auto_randomize then randomizer.roll() end
+                    if config.current.auto_quest.auto_randomize
+                    and #randomizer.filtered_quest_list ~= 0
+                    or not config.current.auto_quest.auto_randomize then
+                        if dump.quest_data_list[tonumber(config.current.auto_quest.quest_no)] then
+                            for k,v in pairs(actions) do actions[k] = true end
+                            vars.quest_type = dump.quest_data_list[tonumber(config.current.auto_quest.quest_no)]['category']
+                            if vars.quest_type ~= 'Random Mystery' then vars.quest_type = "Normal" end
+                            vars.posting = true
+                        else
+                            functions.error_handler("Invalid Quest ID.")
+                        end
                     end
-                    local quest_data = dump.quest_data_list[tonumber(config.current.auto_quest.quest_no)]
-                    if quest_data and random_pool then
-                        session_action_active = false
-                        vars.posting = true
-                        get_menu = true
-                    elseif not quest_data then
-                        functions.error_handler("Invalid Quest ID.")
-                    end
-                elseif not target_name then
+                elseif not quest_counter_obj_loop.name then
                     functions.error_handler("Can't post in this area.")
                 end
             end
@@ -116,40 +114,10 @@ function multiplayer.switch()
 end
 
 function multiplayer.hook()
-    sdk.hook(methods.quest_session_action_update,
-        function(args)
-            if config.current.auto_quest.posting_method == 2 then
-                if vars.posting then
-                    session_action_active = true
-                    if config.current.auto_quest.send_join_request and not req_select_attempt and not vars.selection_trigger and not vars.selected and methods.is_internet:call(nil) then
-                        select_index_selection_window(1)
-                        req_select_attempt = true
-                    elseif config.current.auto_quest.send_join_request and (vars.selected or not methods.is_internet:call(nil)) then
-                        vars.decide_trigger = true
-                        vars.selected = false
-                    elseif vars.selected == nil then
-                        vars.posting = false
-                        vars.cancel_trigger = true
-                        functions.error_handler("Menu selection timeout.")
-                    end
-                end
-            end
-        end
-    )
-
-    sdk.hook(methods.quest_session_action_start,
-        function(args)
-            if config.current.auto_quest.posting_method == 2 then
-                if vars.posting and config.current.auto_quest.send_join_request then
-                    vars.decide_trigger = false
-                end
-            end
-        end
-    )
-
     sdk.hook(methods.check_quest_hr,function(args)end,
         function(retval)
-            if config.current.auto_quest.posting_method == 2 and vars.posting then
+            if config.current.auto_quest.posting_method == 2
+            and vars.posting then
                 return sdk.to_ptr(true)
             else
                 return retval
@@ -157,129 +125,58 @@ function multiplayer.hook()
         end
     )
 
-    sdk.hook(methods.quest_session_action_end,
-        function(args)
-            if config.current.auto_quest.posting_method == 2 then
-                session_action_active = false
-                req_select_attempt = false
-            end
-        end
-    )
-
-    sdk.hook(methods.set_quest_counter_state,function(args)end,
-        function(args)
-            if config.current.auto_quest.posting_method == 2 then
-                if vars.posting and get_menu then
-                    vars.interact_trigger = false
-
-                    if get_quest_counter_menu() then
-                        vars.decide_trigger = true
-                    else
-                        vars.close_trigger = true
-                        functions.error_handler("Can't post chosen quest type.")
-                    end
-                    get_menu = false
-
-                end
-            end
-        end
-    )
-
     sdk.hook(methods.quest_id_reset,
         function(args)
-            if config.current.auto_quest.posting_method == 2 then
-                if config.current.auto_quest.keep_rng then
-                    args[3] = sdk.to_ptr(false)
-                end
+            if config.current.auto_quest.posting_method == 2
+            and vars.posting
+            and config.current.auto_quest.keep_rng then
+                args[3] = sdk.to_ptr(false)
             end
-        end
-    )
-
-    sdk.hook(methods.decide_button,function(args)end,
-        function(retval)
-            if config.current.auto_quest.posting_method == 2 then
-                local current_menu = nil
-                if vars.decide_trigger then
-
-                    current_menu = quest_counter_type_def:get_field('<QuestCounterState>k__BackingField'):get_data()
-
-                end
-                if vars.decide_trigger and (vars.selected or vars.quest_type == 'Normal' and current_menu ~= 0 or vars.quest_type == 'Random Mystery' and current_menu == 32) then
-                    vars.selected = false
-                    return sdk.to_ptr(true)
-
-                elseif vars.decide_trigger and current_menu == 30 and not vars.selection_trigger and not session_action_active then
-                    if not select_mystery_quest() then vars.selected = nil end
-                    return retval
-
-                elseif vars.decide_trigger and vars.selected == nil then
-                    vars.posting = false
-                    vars.decide_trigger = false
-                    vars.close_trigger = true
-                    functions.error_handler("Menu selection timeout.")
-                    return retval
-
-                elseif vars.decide_trigger and ( (not vars.selected and vars.quest_type == 'Normal') or session_action_active) then
-                    return sdk.to_ptr(true)
-
-                else
-                    return retval
-                end
-            else
-                return retval
-            end
-        end
-    )
-
-    sdk.hook(methods.quest_counter_on_destroy,
-        function(args)
-            if config.current.auto_quest.posting_method == 2 then
-                if vars.posting then
-                    vars.posting = false
-                    vars.close_trigger = false
-                    vars.decide_trigger = false
-                end
-            end
-
         end
     )
 
     sdk.hook(methods.pop_sensor_check_access,
         function(args)
-            if config.current.auto_quest.posting_method == 2 and vars.posting then
+            if config.current.auto_quest.posting_method == 2
+            and vars.posting then
                 return sdk.PreHookResult.SKIP_ORIGINAL
             end
         end,
         function(retval)
-            if config.current.auto_quest.posting_method == 2 then
-                if vars.posting and target_name then
-                    local name = nil
-                    local sensor = methods.get_sensor:call(singletons.objaccman,1)
-                    local obj = methods.pop_sensor_get_access_target:call(sensor)
+            if config.current.auto_quest.posting_method == 2
+            and vars.posting
+            and actions.select_quest_counter then
 
-                    if obj then
-                        name = methods.get_gameobject_name:call(obj)
-                        if loop_count == loop_max then
-                            vars.posting = false
-                            target_name = nil
-                            loop_count = 0
-                            functions.error_handler("Failed to find Quest Counter NPC\nMove closer and try again.")
-                            return retval
-                        end
-
-                        if name == target_name then
-                            target_name = nil
-                            vars.interact_trigger = true
-                            loop_count = 0
-                        else
-                            methods.focus_next_target:call(singletons.objaccman)
-                        end
-                    end
-                    loop_count = loop_count + 1
-                    return sdk.to_ptr(true)
-                else
-                    return retval
+                if not quest_counter_obj_loop.sensor then
+                    quest_counter_obj_loop.sensor = methods.get_sensor:call(singletons.objaccman,1)
                 end
+
+                if quest_counter_obj_loop.count == quest_counter_obj_loop.max then
+                    vars.posting = false
+                    actions.select_quest_counter = false
+                    functions.error_handler("Failed to find Quest Counter NPC\nMove closer and try again.")
+                end
+
+                local obj = methods.pop_sensor_get_access_target:call(quest_counter_obj_loop.sensor)
+
+                if obj then
+                    if methods.get_gameobject_name:call(obj) == quest_counter_obj_loop.name then
+                        vars.interact_trigger = true
+                        actions.select_quest_counter = false
+                    else
+                        methods.focus_next_target:call(singletons.objaccman)
+                    end
+                end
+
+                if not actions.select_quest_counter then
+                    quest_counter_obj_loop.name = nil
+                    quest_counter_obj_loop.sensor = nil
+                    quest_counter_obj_loop.count = 0
+                end
+
+                quest_counter_obj_loop.count = quest_counter_obj_loop.count + 1
+
+                return sdk.to_ptr(true)
             else
                 return retval
             end
@@ -288,8 +185,9 @@ function multiplayer.hook()
 
     sdk.hook(methods.interact_button,function(args)end,
         function(retval)
-            if config.current.auto_quest.posting_method == 2 then
-                if vars.interact_trigger then return sdk.to_ptr(true) else return retval end
+            if config.current.auto_quest.posting_method == 2
+            and vars.interact_trigger then
+                return sdk.to_ptr(true)
             else
                 return retval
             end
@@ -298,7 +196,8 @@ function multiplayer.hook()
 
     sdk.hook(methods.is_servant_quest,function(args)end,
         function(retval)
-            if config.current.auto_quest.posting_method == 2 and vars.posting then
+            if config.current.auto_quest.posting_method == 2
+            and vars.posting then
                 return sdk.to_ptr(false)
             else
                 return retval
@@ -306,6 +205,78 @@ function multiplayer.hook()
         end
     )
 
+    sdk.hook(methods.open_all_quest_hud,function(args)end,
+        function(retval)
+            if config.current.auto_quest.posting_method == 2
+            and vars.posting then
+                vars.posting = false
+                vars.decide_trigger = false
+                vars.close_trigger = false
+                singletons.quest_counter = nil
+                if config.current.auto_quest.auto_depart and methods.is_quest_posted:call(singletons.questman) then
+                    methods.go_quest:call(singletons.guiman:get_field('<refQuestStartFlowHandler>k__BackingField'),true)
+                end
+            end
+        end
+    )
+
+    re.on_frame(function()
+        if config.current.auto_quest.posting_method == 2 then
+            if vars.posting then
+
+                if not singletons.quest_counter then
+                    singletons.quest_counter = sdk.get_managed_singleton('snow.gui.fsm.questcounter.GuiQuestCounterFsmManager')
+                end
+
+                if singletons.quest_counter then
+                    vars.interact_trigger = false
+
+                    if vars.selected then
+                        vars.selected = false
+                        vars.decide_trigger = true
+                    elseif vars.selected == nil then
+                        vars.posting = false
+                        vars.close_trigger = true
+                        functions.error_handler("Menu selection timeout.")
+                    end
+
+                    local current_menu = singletons.quest_counter:get_field('<QuestCounterState>k__BackingField')
+
+                    if current_menu == 0
+                    and actions.select_menu then
+                        if not get_quest_counter_top_menu() then
+                            vars.close_trigger = true
+                            functions.error_handler("Can't post chosen quest type.")
+                        end
+                        actions.select_menu = false
+                    end
+
+                    if current_menu ~= 0
+                    and current_menu ~= 30
+                    or
+                    current_menu == 30
+                    and methods.is_open_select:call(singletons.guiman) then
+                        if not methods.is_open_select:call(singletons.guiman) then
+                            vars.decide_trigger = true
+                        elseif config.current.auto_quest.send_join_request
+                        and actions.select_option
+                        and methods.is_open_select:call(singletons.guiman)
+                        and methods.is_internet:call(nil) then
+                            select_index_selection_window(1)
+                            actions.select_option = false
+                        elseif not methods.is_internet:call(nil) then
+                            vars.decide_trigger = true
+                        end
+                    elseif current_menu == 30
+                    and actions.select_mystery then
+                        select_mystery_quest()
+                        actions.select_mystery = false
+                    end
+                end
+            end
+        end
+    end
+    )
 
 end
 
