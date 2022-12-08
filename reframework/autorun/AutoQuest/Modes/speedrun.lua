07a474be6ce11412ce5c1c89e8fbaf8717799af9
kmy0 <93156259+kmy0@users.noreply.github.com>
2022-12-08 06:50:16 +0100

    Create speedrun.lua

---
+++ reframework/autorun/AutoQuest/Modes/speedrun.lua
@@ -0,0 +1,61 @@
+local speedrun = {}
+local config
+local singletons
+local methods
+local functions
+local vars
+local randomizer
+local dump
+
+
+function speedrun.switch()
+    function functions.post_quest()
+        if methods.can_open_quest_board:call(singletons.guiman)
+        and not methods.is_quest_posted:call(singletons.questman) then
+            if config.current.auto_quest.auto_randomize then randomizer.roll() end
+            if config.current.auto_quest.auto_randomize
+            and #randomizer.filtered_quest_list ~= 0
+            or
+            not config.current.auto_quest.auto_randomize then
+                if dump.quest_data_list[tonumber(config.current.auto_quest.quest_no)] then
+
+                    vars.posting = true
+                    singletons.quest_counter = sdk.create_instance('snow.gui.fsm.questcounter.GuiQuestCounterFsmManager')
+
+                    methods.quest_counter_awake:call(singletons.quest_counter)
+                    if not config.current.auto_quest.keep_rng then methods.reset_quest_identifier:call(singletons.quest_counter) end
+                    methods.send_quest_to_questman:call(nil)
+                    methods.quest_counter_on_destroy:call(singletons.quest_counter)
+                    singletons.quest_counter = nil
+
+                    if methods.is_online:call(singletons.lobbyman) then
+                        local qi = sdk.create_instance('snow.LobbyManager.QuestIdentifier'):add_ref()
+                        methods.quest_id_copy_from:call(qi,methods.get_active_quest_id:call(singletons.questman))
+                        methods.create_room:call(singletons.lobbyman,qi)
+                    end
+
+                    vars.posting = false
+                    if config.current.auto_quest.auto_depart
+                    and methods.is_quest_posted:call(singletons.questman) then
+                        methods.go_quest:call(singletons.guiman:get_field('<refQuestStartFlowHandler>k__BackingField'),true)
+                    end
+
+                else
+                    functions.error_handler("Invalid Quest ID.")
+                end
+            end
+        end
+    end
+end
+
+function speedrun.init()
+    singletons = require("AutoQuest.singletons")
+    vars = require("AutoQuest.Common.vars")
+    methods = require("AutoQuest.methods")
+    functions = require("AutoQuest.Common.functions")
+    config = require("AutoQuest.config")
+    randomizer = require("AutoQuest.randomizer")
+    dump = require("AutoQuest.dump")
+end
+
+return speedrun
